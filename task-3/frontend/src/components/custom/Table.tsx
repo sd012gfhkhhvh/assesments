4e2204c949558e5ea220cf1683ea692b0590b9b5
SOHAM DAS <sohamdas.nest@gmail.com>
2024-03-26 22:55:56 +0530

    bug fix

--- task-3/frontend/src/components/custom/Table.tsx
+++ task-3/frontend/src/components/custom/Table.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from "react";
+import { FC, useState } from "react";
 import { ChartWrapper } from "./ChartWrapper";
 import { Header } from "./Header";
 import { tableData } from "@/data/tableData";
